58330654fee1d0f42c9500ceee84f3133500a820
MalinLars <malin.lars@live.se>
2022-11-29 14:56:27 +0100

    First commit

---
+++ src/Age.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Age {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner MyAge = new Scanner(System.in);
+		System.out.println("What is your age:");
+		String Age = MyAge.nextLine();
+		System.out.println("Du är: " +Age+ " år gammal.");
+		
+
+	}
+
+}
